075d82b41c76e8cf3c16912b8158b2dffcbce772
eslamibrahimhamed <eslamiibrahimhamed@gmail.com>
2022-04-16 06:02:09 +0200

    add 16/4/2022

--- src/components/AddItem/AddItems.js
+++ src/components/AddItem/AddItems.js
@@ -1,9 +1,47 @@
 import React, { Component } from "react";
-import './AddItems.css'
+import "./AddItems.css";
 
+const width = {
+  width: "12rem",
+  display: "inline-block",
+};
+
+const verticalAlign = {
+  verticalAlign: "inherit",
+};
 class AddItem extends Component {
   render() {
-    return <div>Add Items</div>;
+    return (
+      <div>
+        <form>
+          <input
+            type="text"
+            className="form-control"
+            name="name"
+            placeholder="Add Name"
+            autoComplete="off"
+            style={width}
+          />
+
+          <input
+            type="number"
+            className="form-control"
+            name="age"
+            placeholder="Add Age"
+            autoComplete="off"
+            style={width}
+          />
+          <button
+            className="btn btn-primary"
+            type="submit"
+            value="Submit"
+            style={verticalAlign}
+          >
+            Add
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
